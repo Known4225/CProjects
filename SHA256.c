4ae115c494581dd0bb2b7bf0cc4653e006934793
Known4225 <83723434+Known4225@users.noreply.github.com>
2023-02-12 16:19:40 -0600

    Update SHA256.c

--- SHA256.c
+++ SHA256.c
@@ -146,7 +146,7 @@ void printBin(unsigned int num, int bitLength) {
     for (int i = 0; i < bitLength; i++) {
         printf("%d", throw[i]);
     }
-    printf("\n");
+    // printf("\n");
     free(throw);
 }
 
@@ -238,7 +238,7 @@ int *sha256(char *input, short print) {
     int ascii[len];
     constants_t constants = generate_constants();
     bool *binOut;
-    if (print == 1) {printf("\nASCII: {");}
+    if (print == 1) {printf("\nASCII: \n{");}
     for (short i = 0; i < len; i++) {
         ascii[i] = (int) input[i];
         if (print == 1) {printf("%d, ", ascii[i]);}
@@ -249,7 +249,7 @@ int *sha256(char *input, short print) {
     short next = 0;
     if (print == 1) {printf("\b\b}\n");}
     if (print == 1) {printf("\nBlocks used: %d\n", numBlocks);}
-    for (short i = 0; i < len && i < 64; i++) {
+    for (short i = 0; i < len; i++) {
         binOut = binary(ascii[i], 8);
         for (short j = 0; j < 8; j++) {
             allBlock[next] = binOut[j];
@@ -277,6 +277,7 @@ int *sha256(char *input, short print) {
         track[i] = constants.squareRoots[8 - 1 - i];
         // printf("C%d: ", i);
         // printBin(track[i], 32);
+        // printf("\n");
     }
     binOut = binary(len * 8, 64);
     for (short i = 0; i < 64; i++) {
@@ -289,48 +290,96 @@ int *sha256(char *input, short print) {
     for (short i = 0; i < next; i++) {
         if (print == 1) {printf("%d", allBlock[i]);}
     }
+    if (print == 1) {printf("\n");}
     for (int m = 0; m < numBlocks; m++) {
-        if (print == 1) {printf("\n\nSchedule:\n");}
+        if (print == 1) {printf("\nBlock %d Schedule:\n", m + 1);}
         for (short i = 0; i < 16; i++) {
             for (short j = 0; j < 32; j++) {
                 schedule[i][j] = allBlock[i * 32 + j + m * 512];
                 // printf("%d", schedule[i][j]);
             }
             schedint[i] = toInt(schedule[i], 32);
-            if (print == 1) {printf("W%-2u: ", i);}
-            if (print == 1) {printBin(schedint[i], 32);}
+            if (print == 1) {
+                printf("W%-2u: ", i);
+                printBin(schedint[i], 32);
+                printf("   K%-2u: ", i);
+                printBin(constants.cubeRoots[i], 32);
+                if (m > 0) {printf("                      |");}
+                printf("\n");
+            }
             // printf("\n");
         }
 
         for (short i = 16; i < 64; i++) {
             schedint[i] = sigma1(schedint[i - 2]) + schedint[i - 7] + sigma0(schedint[i - 15]) + schedint[i - 16];
-            if (print == 1) {printf("W%-2u: ", i);}
-            if (print == 1) {printBin(schedint[i], 32);}
+            if (print == 1) {
+                printf("W%-2u: ", i);
+                printBin(schedint[i], 32);
+                printf("   K%-2u: ", i);
+                printBin(constants.cubeRoots[i], 32);
+                if (m > 0) {printf("                      |");}
+                printf("\n");
+            }
         }
         
-        if (print == 1) {printf("\n");}
+        if (print == 1) {
+            if (m > 0) {printf("                                                                                                   |");}
+            printf("\n");
+        }
         for (short i = 0; i < 8; i++) {
             compress[i] = track[i];
             // printBin(compress[i], 32);
         }
         // printf("K0: ");
         // printBin(constants.cubeRoots[0], 32);
+        // printf("\n");
         for (short i = 0; i < 64; i++) {
             T1 = SIGMA1(compress[3 + i]) + choice(compress[3 + i], compress[2 + i], compress[1 + i]) + compress[0 + i] + constants.cubeRoots[i] + schedint[i];
             T2 = SIGMA0(compress[7 + i]) + maj(compress[7 + i], compress[6 + i], compress[5 + i]);
             compress[8 + i] = T1 + T2;
             compress[4 + i] += T1;
-            // break;
+            if (print == 1) {
+                printf("C%-2d: ", i);
+                printBin(compress[i], 32);
+                if (i < 8) {
+                    printf("   H%-2d: ", i);
+                    printBin(track[i], 32);
+                    if (m > 0) {
+                        if (i == 4) {
+                            printf("   <------------------");
+                        } else if (i < 4) {
+                            printf("                      |");
+                        }
+                    }
+                } else {
+                    if (i > 9 && i < 62) {
+                        printf("                      |");
+                    }
+                    if (i == 62) {
+                        printf("                      V");
+                    }
+                }
+                printf("\n");
+            }
             // printf("T1: ");
             // printBin(T1, 32);
-            // printf("T2: ");
+            // printf("\nT2: ");
             // printBin(T2, 32);
+            // printf("\n");
             // break;
         }
-        for (short i = 7; i >= 0; i--) {
-            track[i] += compress[64 + i];
-            if (print == 1) {printf("C%d: ", 7 - i);}
-            if (print == 1) {printBin(track[i], 32);}
+        if (print == 1) {printf("\n");}
+        for (short i = 0; i < 8; i++) {
+            if (print == 1) {
+                printf("C%d: ", 64 + i);
+                printBin(compress[64 + i], 32);
+                printf(" + H%d: ", i);
+                printBin(track[i], 32);
+                track[i] += compress[64 + i];
+                printf(" = H%d: ", i);
+                printBin(track[i], 32);
+                printf("\n");
+            } else {track[i] += compress[64 + i];}
         }
 
         // printf("\n");
@@ -349,8 +398,27 @@ int *sha256(char *input, short print) {
         // test3 = 0x80000000;
         // unsigned int test3sigma = SIGMA1(toInt(test3bool, 32));
         // printBin(test3sigma, 32);
+        // printf("\n");
     }
-    if (print == 1) {printf("\nFinal Hash:\n");}
+    if (print == 1) {
+        printf("\n");
+        for (short i = 0; i < 8; i++) {
+            printf("F%d: ", i);
+            printBin(track[7 - i], 32);
+            if (i == 3) {
+                printf("   <------------------ reversed (H0 = F7) ---------------------");
+            } else if (i < 3) {
+                printf("                                                               |");
+            } else if (i == 4) {
+                printf("   --------");
+            } else {
+               printf("           |"); 
+            }
+            printf("\n");
+        }
+        printf("                                               V");
+        printf("\nFinal Hash:\n");
+    }
     for (int i = 7; i >= 0; i--) {
         out[7 - i] = track[i];
         if (print >= 1) {printf("%x", track[i]);}
