d522d50507405dc762ac0d19f548826bbe8f4291
Ryan Srichai <83723434+Known4225@users.noreply.github.com>
2023-09-07 12:44:22 -0500

    Add files via upload

---
+++ Algorithms/include/random.h
@@ -0,0 +1,10 @@
+/* custom random library with some helpful randomiser functions */
+
+#include <stdlib.h>
+extern inline int randomInt(int lowerBound, int upperBound) { // random integer between lower and upper bound (inclusive)
+    return (rand() % (upperBound - lowerBound + 1) + lowerBound);
+}
+
+extern inline double randomDouble(double lowerBound, double upperBound) { // random double between lower and upper bound
+    return (rand() * (upperBound - lowerBound) / RAND_MAX + lowerBound); // probably works idk
+}
