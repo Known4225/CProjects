6c5fbf27191219e5e57cd94a6864bf8fe4ba8ea4
Ryan Srichai <ryansrichai@gmail.com>
2023-09-08 16:16:25 -0500

    CSELabs commit

---
+++ Algorithms/3rdlect.c
@@ -0,0 +1,16 @@
+/*
+11.09.23:
+I feel like something happened today but i just can't quite remember
+
+
+*/
+
+/* this file contains: */
+
+#include "include/list.h"
+#include "include/random.h"
+#include <time.h>
+
+int main(int argc, char *argv[]) {
+
+}
